6e41d4526bf619da2d33785ad243f609368353b2
brinda behal <brindab50@gmail.com>
2019-05-17 00:36:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,9 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-		monopoly = {:railroads => {:pieces => 4 }
-		}         :railroads =>  {:name => {}
+		monopoly = {:railroads => {
+		  :pieces => 4 , 
+		  :name => {}
 		}
 end
 
